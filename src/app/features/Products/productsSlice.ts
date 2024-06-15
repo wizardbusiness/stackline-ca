ae3627de6b58fba89a8e1781e6c1b87062d2abb8
Gabriel <gkime91@gmail.com>
2024-06-14 21:04:48 -0700

    added state types to productsSlice

--- src/app/features/Products/productsSlice.ts
+++ src/app/features/Products/productsSlice.ts
@@ -1,6 +1,37 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
+type Sales = {
+  weekEnding: string;
+  retailSales: number;
+  wholesaleSales: number;
+  unitsSold: number;
+  retailerMargin: number;
+}[];
+
+type Reviews = {
+  customer: string;
+  review: string;
+  score: number;
+}[];
+
+type Product = {
+  id: string;
+  title: string;
+  image: string;
+  subtitle: string;
+  brand: string;
+  reviews: Reviews;
+  retailer: string;
+  details: string[];
+  tags: string[];
+  sales: Sales;
+};
+
+type InitialState = {
+  products: Product[];
+};
+
+const initialState: InitialState = {
   products: []
 };
 
