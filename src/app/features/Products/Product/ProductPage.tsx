bae1e0d5b9843f0800d1924588eacf5618a9bb1d
Gabriel <gkime91@gmail.com>
2024-06-15 00:06:43 -0700

    added boilerplate for ProductCard and ProductPage, and fetched product into productpage with rtk query

--- src/app/features/Products/Product/ProductPage.tsx
+++ src/app/features/Products/Product/ProductPage.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { useGetProductByIdQuery } from '../services/productsApi';
 
-const Products = () => {
-  return <div>ProductInfo</div>;
+const ProductsPage = () => {
+  const { data, error, isLoading } = useGetProductByIdQuery('B007TIE0GQ');
+  return <div>{data?.title}</div>;
 };
 
-export default Products;
+export default ProductsPage;
