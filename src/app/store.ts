76123ed4923485eb738971d2762f8e55dd35ff6e
Gabriel <gkime91@gmail.com>
2024-06-15 00:04:01 -0700

    deprecated productSlice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { useDispatch } from 'react-redux';
-import productsReducer from './features/Products/productsSlice';
+import productsApi from './features/Products/services/productsApi';
 
 export const store = configureStore({
   reducer: {
-    productInfo: productsReducer
-  }
+    [productsApi.reducerPath]: productsApi.reducer
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware)
 });
 
 export type AppDispatch = typeof store.dispatch;
