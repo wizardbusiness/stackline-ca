022b291b7dca3191d0e1fe816897b35bcc8bdcd6
Gabriel <gkime91@gmail.com>
2024-06-15 05:58:47 -0700

    added header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,11 @@
+import _React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <img height={150} width={150} src="./stackline_logo.svg"></img>
+    </header>
+  );
+};
+
+export default Header;
